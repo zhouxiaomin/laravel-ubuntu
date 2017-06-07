f003a0f49cf14a0eae30404c29e9694a1bbde447
xiaomin <547654614@qq.com>
2017-06-07 15:53:53 +0800

    ServerFormXiaomin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,8 +40,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
-                <div class="quote">{{ Inspiring::quote() }}</div>
+                <div class="title">ServerForXiaomin</div>
+                {{--<div class="quote">{{ Inspiring::quote() }}</div>--}}
             </div>
         </div>
     </body>
